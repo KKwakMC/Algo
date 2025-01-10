bac27923591ca5637b866b40e7a4c69a8d8d763f
msshin <ngp123@thinkpool.com>
2025-01-10 10:49:10 +0900

    test

---
+++ src/backjunCode/DataStructure/sample.java
@@ -0,0 +1,35 @@
+package backjunCode.DataStructure;
+
+import java.io.IOException;
+
+/*
+ * 아이디어 : 
+ * 	2중 for => 값1 && 방문X => BFS
+ * 	BFS 돌면서 그림개수 +1, 최대값 갱신
+ * 
+ * 
+ * 시간복잡도 : 
+ * 	BFS : O(V+E)
+ * 	V : M*N = 500*500
+ *  E : V*4 = 500*4
+ *  V+E : 5*500*500 = 100만 < 2억 => 가능
+ * 
+ * 
+ * 자료구조 :
+ * 그래프 전체 지도 : int[][]
+ * 방문여부 : bool[][]
+ * Queue
+ * 
+ * 
+ * 
+*/
+
+
+public class sample {
+
+	    public static void main(String args[]) throws IOException{
+	    	System.out.println("test");
+	    }
+
+
+}
