214a377fe2bff6473598b0679adb23ac5da025e2
msshin <ngp123@thinkpool.com>
2025-03-25 19:18:07 +0900

    2025.03.25 Ex1918

---
+++ src/backjunCode/DataStructure/Ex1918.java
@@ -0,0 +1,54 @@
+package backjunCode.DataStructure;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Ex1918 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String expression = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : expression.toCharArray()) {
+            if (Character.isAlphabetic(ch)) {
+                result.append(ch);
+            } else if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result.append(stack.pop());
+                }
+                stack.pop();
+            } else {
+                while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(ch)) {
+                    result.append(stack.pop());
+                }
+                stack.push(ch);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+
+        bw.write(result.toString());
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+
+    public static int precedence(char op) {
+        if (op == '(') return 0;
+        if (op == '+' || op == '-') return 1;
+        if (op == '*' || op == '/') return 2;
+        return -1;
+    }
+}
